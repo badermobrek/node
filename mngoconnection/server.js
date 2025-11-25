c37eda2d2ec09bc3766302705e78a368f83b90fa
badermobrek <badermobrek@gmail.com>
2025-11-25 16:57:52 +0400

    mngconnect

---
+++ mngoconnection/server.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/mydb')
+.then(()=>{console.log('connected')})
+.catch((err)=>{console.error("connection error:", err)});
