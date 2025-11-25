e9392ebfe94e5f7f1a6946d183d1e21399daf08b
badermobrek <badermobrek@gmail.com>
2025-11-25 17:00:43 +0400

    commit

--- mngoconnection/server.js
+++ mngoconnection/server.js
@@ -3,3 +3,10 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/mydb')
 .then(()=>{console.log('connected')})
 .catch((err)=>{console.error("connection error:", err)});
+
+
+// git add .
+
+// git commit -m "mngconnect"
+
+// git push origin main
